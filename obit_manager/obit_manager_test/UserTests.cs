fb657ec63d5631fafa98fcff5af9443840e3f87e
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2018-10-05 10:23:42 +0200

    Delete old UserTest.cs.

---
+++ obit_manager/obit_manager_test/UserTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using obit_manager_api;
+
+
+namespace obit_manager_test
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void TestUserOperations()
+        {
+            // Create a user
+            LocalUserManager.CreateUser("aaron", "password");
+        }
+    }
+}
