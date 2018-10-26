9e1616b978e8bb71d9fdd65e16555439dbf471f6
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2018-10-26 16:17:36 +0200

    Add shell class to run process as any user and corresponding tests.

--- obit_manager/obit_manager_test/LocalUserTests.cs
+++ obit_manager/obit_manager_test/LocalUserTests.cs
@@ -13,8 +13,9 @@ namespace obit_manager_test
         [TestMethod]
         public void TestUserOperations()
         {
-            // User name
+            // User credentials
             string username = "obit_test_user";
+            string password = "Areh45wldy8d";
 
             // If the user already exists, delete it first
             if (LocalUserManager.UserExists(username))
@@ -25,7 +26,7 @@ namespace obit_manager_test
             }
 
             // Create a test user with password that never exprires
-            bool success = LocalUserManager.CreateUser(username, "Areh45wldy8d",
+            bool success = LocalUserManager.CreateUser(username, password,
                 out string errorMessage, "oBIT Test User", "Users", true);
             Assert.AreEqual(success, true, errorMessage);
 
@@ -33,9 +34,30 @@ namespace obit_manager_test
             Assert.AreEqual(LocalUserManager.UserExists(username), true,
                 "Could not find the newly created user!");
 
+            // Run a command as the user we just created
+            System.Security.SecureString securePassword = new System.Security.SecureString();
+            foreach (char c in password)
+            {
+                securePassword.AppendChar(c);
+            }
+            string output = Shell.RunAs("whoami.exe", "", username, securePassword, "");
+            output = output.Replace(System.Environment.NewLine, "");
+            string[] parts = output.Split('\\');
+            Assert.AreEqual(parts[0].ToLower(), System.Environment.MachineName.ToLower(),
+                "Retured user name is not the one expected.");
+            Assert.AreEqual(parts[1].ToLower(), username.ToLower(),
+                "Retured user name is not the one expected.");
+
             // Clean up after creation
             Assert.AreEqual(LocalUserManager.DeleteUser(username),
                 true, "Could not delete user!");
+
+            // Run a command as the current user
+            output = Shell.Run("whoami.exe", "");
+            output = output.Replace(System.Environment.NewLine, "");
+            Assert.AreEqual(output.ToLower(), 
+                System.Security.Principal.WindowsIdentity.GetCurrent().Name.ToLower(),
+                "Retured user name is not the one expected.");
         }
     }
 }
