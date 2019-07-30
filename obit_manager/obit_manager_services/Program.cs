afa08c5c4f04aaa89a85b8c924167a265a9c7fd8
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2019-07-30 17:34:00 +0200

    Add initial Windows Service project to solution to replace JSL down the road.

---
+++ obit_manager/obit_manager_services/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace obit_manager_services
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new DatamoverService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
