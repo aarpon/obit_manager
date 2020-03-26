50f53c7814903b0c2c9a15655457d601021fe3cc
Aaron Ponti <aarpon@gmail.com>
2020-03-26 18:58:45 +0100

    Add an initial draft of a Server Configuration dialog.

---
+++ obit_manager/obit_manager_gui/ServerConfigurationDialog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace obit_manager_gui
+{
+    public partial class ServerConfigurationDialog : Form
+    {
+        public ServerConfigurationDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGenerateCryptoKey_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
